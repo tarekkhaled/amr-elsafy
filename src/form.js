b5273864b279099f5c8bbd5db85269a1d37180fb
tarek khaled <tarekt820@gmail.com>
2019-08-04 02:52:10 +0300

    create inputField function , refactor updateStudentFormINState

--- src/form.js
+++ src/form.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import {checkFromFireBase}from './form_helpers'
+import {checkFromFireBase,failedRegister}from './helpers'
 import validator from 'validator' ;
 import './css/form.css';
 
@@ -11,7 +11,7 @@ function fakeFunction () {
 class Form extends Component {
     state = { 
         databaseCheck : (this.props.databaseCheck) ? true : false ,
-        login_form_student : {
+        login : {
             student_mail : '',
             student_password : '',
             error_student_mail : null , // 1 :: means is empty , 2 :: means is not vaild
@@ -30,7 +30,7 @@ class Form extends Component {
     handleWhichForm= (e) => {
         const {form} = this.props ; 
         switch (form) {
-            case 'login_form_student':
+            case 'login':
                     this.updateStudentForm(e);
                 break;
         
@@ -39,80 +39,39 @@ class Form extends Component {
         }
     }
     
+    updateStudentFormINState = (e,prop,value,boolvalue) => {
+        this.setState({
+            login : {...this.state.login,[prop] : value ,'submitSucces' : boolvalue ,[e.target.name] : e.target.value}
+        })
+    }
 
     /* Input :: event  || Which Props Depend :: None */
     /* Place For calling the function :: handleWhichForm (e) */
     /* Depend on this.State (Object of "form Name coming from Props" and it's properties) */
     /* This Function handle client-server-errors and update every state for all inputs of the field when changed  */
-
+    
     updateStudentForm = (e) => {
-        const {login_form_student} = this.state; 
+        const {login} = this.state; 
         console.log(e.target.name)
         switch (e.target.name) {
             case "student_mail":
-                if(!e.target.value) {
-                    this.setState({
-                        login_form_student : {
-                            ...this.state.login_form_student,
-                            error_student_mail :  "البريد الالكتروني مطلوب",
-                            [e.target.name]: e.target.value,
-                            submitSucces : false
-                        }
-                    })
-                }
-                else if(!validator.isEmail(e.target.value)) {
-                    this.setState({
-                        login_form_student : {
-                            ...this.state.login_form_student,
-                            error_student_mail :  'تاكد من البريد الالكتروني',
-                            [e.target.name]: e.target.value,
-                            submitSucces : false
-
-                        }
-                    })
-
-                } else {
-                    this.setState({submitSucces : true})
-                    this.setState({
-                        login_form_student : {
-                            ...this.state.login_form_student,
-                            error_student_mail :  'no',
-                            [e.target.name]: e.target.value, 
-                            submitSucces : (login_form_student.error_student_password === 'no') ? true : false 
-                        }
-                    })
-                    
-                }
-             
+                if(!e.target.value) 
+                    this.updateStudentFormINState(e,'error_student_mail',"البريد الالكتروني مطلوب", false);
+                else if(!validator.isEmail(e.target.value)) 
+                    this.updateStudentFormINState(e,'error_student_mail','تاكد من البريد الالكتروني', false);
+                else 
+                    this.updateStudentFormINState(e,'error_student_mail','no', (login.error_student_password === 'no') ? true : false);
                 break;
             case "student_password" : // empty string 
-                if(!e.target.value) {
-                    this.setState({
-                        login_form_student : {
-                            ...this.state.login_form_student,
-                            error_student_password :  'كلمة السر مطلوبة',
-                            [e.target.name]: e.target.value,
-                            submitSucces : false
-                        }
-                    })
-                } else {
-                    this.setState({
-                        login_form_student : {
-                            ...this.state.login_form_student,
-                            error_student_password :  'no',
-                            [e.target.name]: e.target.value,
-                            submitSucces : ((login_form_student.error_student_mail=== 'no') ? true : false)
-                        }
-                    })
-                }
+                if(!e.target.value) 
+                    this.updateStudentFormINState(e,'error_student_password','كلمة السر مطلوبة', false);
+                else 
+                this.updateStudentFormINState(e,'error_student_password','no', (login.error_student_mail=== 'no') ? true : false);  
             break;
             default:
                 break;
         }
-
     }
-
-
     
     /* Input :: event  || Which Props Depend :: All */
     /* Place For calling the function :: render () */
@@ -127,6 +86,7 @@ class Form extends Component {
         for (let key in this.props) {
             if(`${key}`.includes('_field'))
             {
+                console.log(this.props[key]);
                 const inputvalue_part2 = this.props[key].input_name;
                 const inputvalue_error = this.props[key].error
                 inputsToShown.push(
@@ -145,20 +105,18 @@ class Form extends Component {
             return inputsToShown
     }
     
-
-    
     /* Input :: event  || Which Props Depend :: None */
     /* Place For calling the function :: render () */
     /* Depend on States */
     /* This Function handle Will check if Form Vaild will redirect to Profile Page  */
 
     handleSubmit= (e) => {
-        const {login_form_student,databaseCheck} = this.state;
+        const {login,databaseCheck} = this.state;
         e.preventDefault();
         if(databaseCheck) {
-            if(login_form_student.submitSucces ) {
-                /*  login_form_student.student_mail  --> final user name to send it to firebase
-                login_form_student.student_password -->   final user password  to send it to firebase */
+            if(login.submitSucces ) {
+                /*  login.student_mail  --> final user name to send it to firebase
+                login.student_password -->   final user password  to send it to firebase */
 
                 /* Replace function below "fakeFunction" with the function from firebase and remove the fake function line 6 because it will be garbage function :"D */
 
@@ -166,6 +124,9 @@ class Form extends Component {
                     (fakeFunction)() ,'./')
 
             } 
+            else {
+                failedRegister()
+            }
         }
 
         // for all others forms will be implement later
