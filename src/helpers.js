62781d257e48b3197e319bd172273d7da6cc82ab
tarek khaled <tarekt820@gmail.com>
2019-08-04 08:31:16 +0300

    add some functions for additions page

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import swal from 'sweetalert';
+
 
 function createInputField (label,type,name,placeholder,errorPlace) {
     return {
@@ -8,9 +10,34 @@ function createInputField (label,type,name,placeholder,errorPlace) {
         placeholder : placeholder,
         error : errorPlace
     }
+
 }
 
 
+function createNav_li (textContent,active=false,num) {
+    return <li  key={num}className={`Additions-card-box ${active ?'Addition-card-box-active' : ''}`}>
+        <i className="fas fa-plus fa-1x"></i>
+        <p>{textContent}</p>
+    </li>
+}
+
+
+function boss_nav (active) {
+    const array = [createNav_li('اضافة اسيسنت',(active === 'assistant'),0),
+    createNav_li('اضافة طالب',(active === 'student'),1),
+    createNav_li('اضافة سنتر',(active === 'center'),2),
+    createNav_li('اضافة مجموعة',(active === 'session'),3) ]
+
+    return array;
+}
+
+function assistant_nav (active) {
+    const array = [
+        createNav_li('اضافة طالب',(active === 'student'),1),
+        createNav_li('اضافة سنتر',(active === 'center'),2),
+        createNav_li('اضافة مجموعة',(active === 'session'),3) ]
+    return array ;
+}
 function checkFromFireBase(firebaseMessage,pathtoGoIFSuccess) {
     if(firebaseMessage.length > 0) {
         failedRegister();
@@ -39,4 +66,4 @@ function failedRegister () {
 }
 
 
-export {createInputField,checkFromFireBase,failedRegister} ;
+export {createInputField,checkFromFireBase,failedRegister,boss_nav,assistant_nav} ;
