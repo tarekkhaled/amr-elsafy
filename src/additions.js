e8dfe5f432badb0feed99ed0121ce759410effce
tarek khaled <tarekt820@gmail.com>
2019-08-04 08:29:46 +0300

    make the first look,logic for the interface forms handling is left

--- src/additions.js
+++ src/additions.js
@@ -1,15 +1,53 @@
 import React , {Component} from 'react';
 import './css/additions.css';
+import Form from './form' ; 
+import {createInputField,boss_nav,assistant_nav} from './helpers';
 
 
-class Login extends Component {
+class Additions extends Component {
+    state = {
+        whichForm : 'add_student',
+        whoEnter : 'boss', // boss , assistant  , student 
+        active : 'session'
+    }
+    sendWhichForm = () => {
+        const {whichForm} = this.state;
+        switch (whichForm) {
+            case 'add_student':
+                return <Form 
+                    form =  'login'
+                    input_field10 = {createInputField(': البريد الالكتروني','email','student_mail','... البريد الالكتروني','error_student_mail')}
+                />              
+            default:
+                break;
+        }
+    }
+
+    handleWhichNavToShow = () => {
+        console.log('e')
+        const {whoEnter,active} = this.state ;
+        switch (whoEnter) {
+            case 'boss':
+                 return boss_nav(active);
+            case 'assistant':
+                 return assistant_nav(active);
+            default:
+                break;
+        }
+    }
     render () {
         return (
-            <h1>additions</h1>
-
+            <div className="Additions">
+                <ul className="Additions-cards">
+    
+                    {this.handleWhichNavToShow()}
+                  
+                </ul> 
+            </div>
         )
     }
 }
 
 
-export default Login ; 
+export default Additions ; 
+
