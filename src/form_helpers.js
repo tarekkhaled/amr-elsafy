5a59a138aa784e06b6a3b154dc1a7310979c49e4
tarek khaled <tarekt820@gmail.com>
2019-08-05 07:34:06 +0300

    refactor the old helpers and divide it for two separte files

---
+++ src/form_helpers.js
@@ -0,0 +1,55 @@
+import swal from 'sweetalert';
+
+
+function createInputField (label,type,name,placeholder,errorPlace) {
+    return {
+        label_name : label,
+        type : type,
+        input_name : name,
+        placeholder : placeholder,
+        error : errorPlace
+    }
+
+}
+
+
+function vaildSubmitFields (obb,currentField) {
+    let checkFlag = true ; 
+    for (const key in obb) {
+            if(!key.includes(currentField) && key.includes('error')) {
+                checkFlag = obb[key] === 'no' ? true : false ;
+            }
+    }
+    return checkFlag ;
+}
+
+function checkFromFireBase(firebaseMessage,pathtoGoIFSuccess) {
+    if(firebaseMessage.length > 0) {
+        failedRegister();
+    }
+    else {
+        succesRegister(pathtoGoIFSuccess)
+    }
+
+}
+function succesRegister (pathtoGo,messageFB) {
+    
+    console.log(messageFB)
+    swal({
+        title: "Good job!",
+        text: (!messageFB)? "تم التسجيل الدخول بنجاح" : messageFB ,
+        icon: "success",
+        button: false,
+      });
+    setTimeout(() => {
+        window.location.assign(pathtoGo) /* will be updated to Profile for user,assistant,amrElsafiy */
+    }, 2000);
+}
+
+
+function failedRegister () {
+    swal('!!! راجع حسابك و كلمة السر',)
+}
+
+
+export {createInputField,checkFromFireBase,failedRegister,vaildSubmitFields,succesRegister} ;
