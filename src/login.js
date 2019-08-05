0d9f380ced7b54e2db15dd18eaee09783a07b323
tarek khaled <tarekt820@gmail.com>
2019-08-05 07:37:59 +0300

    change some styling

--- src/login.js
+++ src/login.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
 import Form from './form';
-import {createInputField} from './helpers';
+import {createInputField} from './form_helpers';
 import './css/login.css';
 
 class Login extends Component {
